9599946e9f8bf6a5ef0388065af37e7da4cdbcc3
SamTheMan6978 <samisheikh450@gmail.com>
2023-06-09 21:28:04 +0400

    Javascript Fixes

--- js/sc.js
+++ js/sc.js
@@ -1,17 +1,23 @@
-function mover(obj) {
-  const description = obj.getAttribute('data-description');
-  const coachInfoContainer = document.querySelector('.coach-info-container');
-  const coachInfo = document.createElement('div');
-  coachInfo.classList.add('coach-info');
-  coachInfo.innerHTML = description;
+document.addEventListener('DOMContentLoaded', function() {
+  const coachIcons = document.querySelectorAll('.coach-icon');
 
-  coachInfoContainer.innerHTML = '';
-  coachInfoContainer.appendChild(coachInfo);
-  coachInfoContainer.classList.add('active');
-}
+  coachIcons.forEach(function(icon) {
+    icon.addEventListener('mouseover', function() {
+      const description = icon.getAttribute('data-description');
+      const coachInfoContainer = document.querySelector('.coach-info-container');
+      const coachInfo = document.createElement('div');
+      coachInfo.classList.add('coach-info');
+      coachInfo.innerHTML = description;
 
-function mout(obj) {
-  const coachInfoContainer = document.querySelector('.coach-info-container');
-  coachInfoContainer.innerHTML = '';
-  coachInfoContainer.classList.remove('active');
-}
+      coachInfoContainer.innerHTML = '';
+      coachInfoContainer.appendChild(coachInfo);
+      coachInfoContainer.classList.add('active');
+    });
+
+    icon.addEventListener('mouseout', function() {
+      const coachInfoContainer = document.querySelector('.coach-info-container');
+      coachInfoContainer.innerHTML = '';
+      coachInfoContainer.classList.remove('active');
+    });
+  });
+});
