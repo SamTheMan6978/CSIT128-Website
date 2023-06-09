1bd8f2dcaf115acd3b2189919708443c6544604a
layannnnn <135433775+layannnnn@users.noreply.github.com>
2023-06-09 14:42:27 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,1 @@
-window.addEventListener('scroll', function() {
-    var footer = document.querySelector('.footer');
-    var footerHeight = footer.offsetHeight;
-    var windowHeight = window.innerHeight;
-    var scrollY = window.scrollY || window.pageYOffset;
-  
-    var bottomOfPage = scrollY + windowHeight >= document.body.scrollHeight;
-  
-    if (bottomOfPage) {
-      footer.classList.add('show');
-    } else {
-      footer.classList.remove('show');
-    }
-  });
+
