4ff4ec622c5e533d49b794b434fbe29366ff0e97
Sami Sheikh <samisheikh@mail.ru>
2023-06-26 08:26:28 +0400

    Changes to html referencing

--- js/register.js
+++ js/register.js
@@ -1,22 +1,89 @@
-document.getElementById("registrationForm").addEventListener("submit", function(event) {
-    event.preventDefault(); 
-    var firstName = document.getElementById("firstName").value;
-    var lastName = document.getElementById("lastName").value;
-    var email = document.getElementById("email").value;
-    var phoneNumber = document.getElementById("phoneNumber").value;
-    var membershipType = document.getElementById("membershipType").value;
-    alert("Registration successful!");
-    document.getElementById("registrationForm").reset();
-  });
-var pass1 = document.getElementById("password");
-var pass2 = document.getElementById("confirm password");
-function confirmPassword(){
-  if(pass1!=pass2){
-    alert("Passwords do not match!");
-  }
-  else{
-    alert("Passwords match!");
-  }
-}
+const http = require('http');
+const url = require('url');
+const querystring = require('querystring');
+const mysql = require('mysql');
 
+// Create MySQL connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '1234',
+  database: 'csit128_project'
+});
 
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL: ' + err.stack);
+    return;
+  }
+  console.log('Connected to MySQL as ID: ' + connection.threadId);
+});
+
+// Create server
+const server = http.createServer((req, res) => {
+  const parsedUrl = url.parse(req.url);
+  const path = parsedUrl.pathname;
+  const queryParams = querystring.parse(parsedUrl.query);
+
+  if (req.method === 'GET' && path === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(`<html><body><form action="/register" method="POST">
+      <input type="text" name="firstName" placeholder="First Name" required><br>
+      <input type="text" name="lastName" placeholder="Last Name" required><br>
+      <input type="email" name="email" placeholder="Email" required><br>
+      <input type="text" name="phoneNumber" placeholder="Phone Number" required><br>
+      <input type="text" name="membershipType" placeholder="Membership Type" required><br>
+      <input type="password" name="password" placeholder="Password" required><br>
+      <input type="submit" value="Register">
+    </form></body></html>`);
+  } else if (req.method === 'POST' && path === '/register') {
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk.toString();
+    });
+    req.on('end', () => {
+      const { firstName, lastName, email, phoneNumber, membershipType, password } = querystring.parse(body);
+      const sql = 'INSERT INTO users (firstName, lastName, email, phoneNumber, membershipType, password) VALUES (?, ?, ?, ?, ?, ?)';
+      const values = [firstName, lastName, email, phoneNumber, membershipType, password];
+
+      connection.query(sql, values, (err, result) => {
+        if (err) {
+          console.error('Error inserting data into MySQL: ' + err.stack);
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end('Error registering user.');
+        } else {
+          console.log('User registered successfully.');
+          req.session.userId = result.insertId;
+          res.writeHead(302, { 'Location': '/success' });
+          res.end();
+        }
+      });
+    });
+  } else if (req.method === 'GET' && path === '/success') {
+    if (req.session.userId) {
+      const sql = 'SELECT * FROM user_info WHERE user_id = ?';
+      connection.query(sql, [req.session.userId], (err, result) => {
+        if (err) {
+          console.error('Error retrieving user data from MySQL: ' + err.stack);
+          res.writeHead(500, { 'Content-Type': 'text/plain' });
+          res.end('Error retrieving user data.');
+        } else {
+          const user = result[0];
+          res.writeHead(200, { 'Content-Type': 'text/plain' });
+          res.end('Registration successful! User details: ' + JSON.stringify(user));
+        }
+      });
+    } else {
+      res.writeHead(302, { 'Location': '/' });
+      res.end();
+    }
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404 Not Found');
+  }
+});
+
+// Start server
+server.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
